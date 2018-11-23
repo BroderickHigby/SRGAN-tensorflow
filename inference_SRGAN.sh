2bd389d9356fe496aeb7287d892726315b6744f3
EC2 Default User <ec2-user@ip-172-31-39-157.us-west-2.compute.internal>
2018-11-23 07:42:40 +0000

    adding pictures

--- inference_SRGAN.sh
+++ inference_SRGAN.sh
@@ -5,8 +5,9 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --mode inference \
     --is_training False \
     --task SRGAN \
-    --input_dir_LR ./data/test_LR/ \
+    --input_dir_LR ./data/test_HR/ \
     --num_resblock 16 \
     --perceptual_mode VGG54 \
     --pre_trained_model True \
+    --TF_CPP_MIN_LOG_LEVEL=2\
     --checkpoint ./SRGAN_pre-trained/model-200000
